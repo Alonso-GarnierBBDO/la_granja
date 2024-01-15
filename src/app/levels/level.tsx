3e74dffa3f178658ecd00670423996e8b27c2803
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2024-01-15 17:22:00 -0600

    Se agrego el icono de la granja

--- src/app/levels/level.tsx
+++ src/app/levels/level.tsx
@@ -1,10 +1,14 @@
 
+import Logo from '../../assets/img/logo.png'
 
 function Level() {
 
     return (
       <>
         <section className="allLevels">
+          <section className="logo">
+            <img src={Logo} alt='Logo la granja'/>
+          </section>
           <section className="silueta"></section>
           <section className="vallas"></section>
         </section>
