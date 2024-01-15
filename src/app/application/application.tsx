f64a2a6956944e59c8720459d2eeb23a4a567219
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2024-01-15 14:54:04 -0600

    Se agrego el fullScreen

--- src/app/application/application.tsx
+++ src/app/application/application.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 import homeConfig from "../../hooks/home"
 
@@ -12,15 +12,47 @@ function Application() {
 
     const canva : RefCanva = useRef< HTMLCanvasElement | null >(null)
 
+    const [full, setFull] = useState<boolean>(false);
+    const [textFull, setTextFull] = useState<string>('Expand window');
+
     useEffect( () => {
       if(canva.current){
         homeConfig(canva.current);
       }
+
+      window.onresize = () => {
+
+        if(canva.current){
+          homeConfig(canva.current);
+        }
+      }
+
     }, [])
+
+    function expandWindow(){
+
+      const documentElement : Document = document;
+      const bodyElement : HTMLElement = document.body;
+
+      if(full){ 
+        documentElement.exitFullscreen();
+        setFull(false);
+        setTextFull('Expand window')
+      }else{
+        bodyElement.requestFullscreen()
+        setFull(true);
+        setTextFull('Reduce window')
+      }
+
+    }
 
     return (
       <>
         <canvas id="canva" ref={canva}></canvas>
+
+          <button id="expand_window" onClick={expandWindow}> { textFull } </button>
+
+      
       </>
     )
   }
