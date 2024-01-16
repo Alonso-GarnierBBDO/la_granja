2e2a0c1587d480b72e7d611f0f90bc4755c845c2
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2024-01-15 18:21:41 -0600

    Se agrego la parrilla

---
+++ src/app/levels/all/level_one.tsx
@@ -0,0 +1,19 @@
+import Parrilla from '../../../assets/img/parrilla.png';
+
+function LevelOne(){
+
+    return (
+        <>
+            <section>
+                <section className="parrilla">
+                    <section>
+                        <img src={Parrilla} alt="Parrilla imagen" />
+                    </section>
+                </section>
+            </section>
+        </>
+    )
+
+}
+
+export default LevelOne;
--- src/app/levels/level.tsx
+++ src/app/levels/level.tsx
@@ -1,5 +1,6 @@
 
-import Logo from '../../assets/img/logo.png'
+import Logo from '../../assets/img/logo.png';
+import LevelOne from './all/level_one';
 
 function Level() {
 
@@ -11,6 +12,9 @@ function Level() {
           </section>
           <section className="silueta"></section>
           <section className="vallas"></section>
+          <section className='level'>
+            <LevelOne/>
+          </section>
         </section>
       </>
     )
