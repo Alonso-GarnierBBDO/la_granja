395ec186479163bfee5accdd387a1902ea5160ad
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2024-01-17 12:31:45 -0600

    Se agrego la info

--- src/app/levels/level.tsx
+++ src/app/levels/level.tsx
@@ -11,6 +11,14 @@ function Level() {
             <img src={Logo} alt='Logo la granja'/>
           </section>
           <section className="silueta"></section>
+          <section className='time'>
+              <div className='chronometer'>
+                <span className='segundos'>00</span>:<span className='milisegundos'>00</span>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore adipisci quae, ipsam rem quos commodi amet numquam nemo, quaerat iure cumque fugit tempora veritatis? Ipsam sapiente inventore adipisci porro magni?</p>
+              </div>
+          </section>
           <section className="vallas"></section>
           <section className='level'>
             <LevelOne/>
--- src/components/carnes/carnes.tsx
+++ src/components/carnes/carnes.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState} from 'react';
 import Cruda from '../../assets/img/crudo.png';
+import Cocido from '../../assets/img/cocido.png';
 
 
 function CarnesComponent(){
 
     const carmeElement = useRef<HTMLImageElement | null>(null);
+    const [parrilla, setParrilla] = useState(false);
+    const [carneCocinada, setCarneCocinada] = useState(false);
+    const [carneWidth, setCarneWidth] = useState<number>(0);
 
     useEffect(() => {
 
@@ -42,7 +46,6 @@ function CarnesComponent(){
                 const heightElement : number = baseImg.offsetHeight;
                 let platoLeft = platoRect.left + ((platoRect.width / 2) - ((heightElement / 2)));
                 const topPosition : number = baseRect.top + (screen.width * 0.023);
-                // const imageLeft = screen.width * 0.04;
                 const carne = carmeElement.current;
 
                 console.log(platoLeft)
@@ -52,12 +55,15 @@ function CarnesComponent(){
 
                     if(screen.width >= 1600){
                         carne.width = heightElement - 40;
+                        setCarneWidth(heightElement - 60 )
                         carne.style.left = `${platoLeft - 20}px`;
                     }else if(screen.width >= 1150){
                         carne.width = heightElement - 20;
+                        setCarneWidth(heightElement - 30);
                         carne.style.left = `${platoLeft - 10}px`;
                     }else{
                         carne.width = heightElement - 10;
+                        setCarneWidth(heightElement - 15);
                         carne.style.left = `${platoLeft - 5}px`;
                     }
 
@@ -71,12 +77,110 @@ function CarnesComponent(){
 
     }
 
+    const clickEffect = () => {
+
+        const getFuego : HTMLImageElement | null = document.querySelector('img.llamasImg');
+        const baseImgRight : HTMLImageElement | null = document.querySelector('.bases.right img');
+        const platilloElementRight : HTMLImageElement | null = document.querySelector('.platos.right img');
+        const carne : HTMLImageElement | null = carmeElement.current
+        const contador : HTMLParagraphElement | null = document.querySelector('.time .chronometer');
+
+        if(getFuego && carne && baseImgRight && platilloElementRight && contador){
+
+            if(!parrilla){
+
+                const fuegoRect = getFuego.getBoundingClientRect();
+                const xFuego = fuegoRect.x + (carne.offsetWidth / 2);
+                const yFuego = fuegoRect.y + (carne.offsetHeight / 2);
+
+                carne.style.left = `${xFuego}px`;
+                carne.style.top = `${yFuego}px`;
+                carne.style.width = `12%`;
+                let milisegundos = 0;
+                let segundos = 7;
+                const milisegundoDiv : HTMLElement = contador.querySelector('.milisegundos') as HTMLElement;
+                const segundosDiv : HTMLElement = contador.querySelector('.segundos') as HTMLElement;
+
+                // console.log(contador);
+
+                const deleteInternal = setInterval(() => {
+    
+                    if(segundos >= 0){
+                        milisegundoDiv.innerHTML = `${milisegundos <= 9 ? '0' + milisegundos : milisegundos  }`;
+                        segundosDiv.innerHTML = `${segundos <= 9 ? '0' + segundos : segundos  }`
+                    }else{
+                        clearInterval(deleteInternal);
+                    }
+
+                    if(milisegundos === 0){
+                        milisegundos = 59
+                        segundos --;
+                    }else {
+                        milisegundos --;
+                    }
+
+                }, 17);
+
+                setParrilla(true);
+
+                setTimeout(() => {
+                    changeCarne(carne);
+                }, 7000)
+
+            }
+
+            if(carneCocinada){  
+
+                const baseRect = baseImgRight.getBoundingClientRect();
+                const platoRect = platilloElementRight.getBoundingClientRect();
+                const heightElement : number = baseImgRight.offsetHeight;
+                let platoLeft = platoRect.left + ((platoRect.width / 2) - ((heightElement / 2)));
+                const topPosition : number = baseRect.top + (screen.width * 0.023);
+
+                carne.style.width = `${carneWidth}px`;
+
+
+                if(carne){
+
+                    if(screen.width >= 1600){
+                        carne.width = heightElement - 40;
+                        setCarneWidth(heightElement - 60 )
+                        carne.style.left = `${platoLeft - 20}px`;
+                    }else if(screen.width >= 1150){
+                        carne.width = heightElement - 20;
+                        setCarneWidth(heightElement - 30);
+                        carne.style.left = `${platoLeft - 10}px`;
+                    }else{
+                        carne.width = heightElement - 10;
+                        setCarneWidth(heightElement - 15);
+                        carne.style.left = `${platoLeft - 5}px`;
+                    }
+
+                    carne.style.top = `${topPosition}px`;
+                    carne.style.left = `${platoLeft}px`;
+
+                }
+
+
+                
+                
+            }
+
+
+        }
+
+    }
+
+    const changeCarne = (carne : HTMLImageElement) => {
+        carne.src = Cocido;
+        setCarneCocinada(true);
+    }
 
     return (
         <>
             <section className="carnes">
                 <section>
-                    <img src={Cruda} alt="carne" ref={carmeElement} />
+                    <img src={Cruda} alt="carne" onClick={clickEffect} ref={carmeElement} />
                 </section>
             </section>
         </>
